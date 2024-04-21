ff85cc7c273170c16d998c16776201919d8c1b42
Copilot0822 <copilot0822@gmail.com>
2024-04-21 15:54:51 -0400

    updated wpi version to 2024.3.2 and went to logged robot

--- SwerveWithPathPlanneredit/src/main/java/frc/robot/Robot.java
+++ SwerveWithPathPlanneredit/src/main/java/frc/robot/Robot.java
@@ -4,13 +4,15 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LoggedRobot;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
